989065f3161d8c289c757eab145399bcf72da688
Kevin Nguyen <KevinNguyen09161995@gmail.com>
2023-12-08 14:32:05 -0600

    Boiler Plate for Django DRF API

---
+++ src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.css']
+})
+export class LandingPageComponent implements OnInit {
+  weaponData: any;
+  url: string = 'http://127.0.0.1:8000/api/weapon/ranged';
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get(this.url).subscribe((res: any) => {
+      this.weaponData = res;
+      console.log(this.weaponData);
+    });
+  }
+}
