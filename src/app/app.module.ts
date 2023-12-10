f94889bcd26cdd027d4066f3af6ec9041b11829f
FireWingLabs-Kevin <KevinNguyen09161995@gmail.com>
2023-12-10 02:25:32 -0600

    Added Player Stats and Search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PlayerSearchComponent } from './player-search/player-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
     NavbarComponent,
+    PlayerSearchComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     NgbModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-landing-page',
@@ -7,15 +6,8 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent implements OnInit {
-  weaponData: any;
-  url: string = 'http://127.0.0.1:8000/api/weapons/';
+  constructor() { }
 
-  constructor(private http: HttpClient) { }
-
-  ngOnInit(): void {
-    this.http.get(this.url).subscribe((res: any) => {
-      this.weaponData = res;
-      console.log(this.weaponData);
-    });
+  ngOnInit(): void {;
   }
 }
