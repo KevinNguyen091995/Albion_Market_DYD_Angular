b9c2eda631b88827b88a9fa67ce15a0550c28a5b
FireWingLabs-Kevin <KevinNguyen09161995@gmail.com>
2023-12-10 15:30:19 -0600

    Added Selected able categories

--- src/app/price-check-page/price-check-page.component.ts
+++ src/app/price-check-page/price-check-page.component.ts
@@ -7,24 +7,54 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./price-check-page.component.css'],
 })
 export class PriceCheckPageComponent implements OnInit {
-  showDropdown = false;
-  weapon_data: any[] = [];
-  item_class: Set<string> = new Set();
-  item_category: Set<string> = new Set();
   api_url: string = 'http://127.0.0.1:8000/api/weapons/';
+  weapon_data: any[] = [];
+  selectedClass: string = '';
+  selectedCategory: string = '';
+  selectedTier: string = '';
+
+  item_class: { [key: string]: string } = {};
+  item_unique_class: Set<any> = new Set<any>();
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.http.get(this.api_url).subscribe((searchRes: any) => {
       searchRes.forEach((item: any) => {
-        this.item_class.add(item.item_class);
-        this.item_category.add(item.item_category);
+        this.item_class[item.item_category] = item.item_class;
       });
+
+      this.updateUniqueValues();
     });
   }
 
-  toggleDropdown() {
-    this.showDropdown = !this.showDropdown;
+  private updateUniqueValues(): void {
+    this.item_unique_class = new Set(Object.values(this.item_class));
+  }
+
+  getItemClassValues(): any[] {
+    let selected_weapons: string[] = [];
+    const data = Object.keys(this.item_class);
+
+    data.forEach((key) => {
+      if (this.item_class[key] == this.selectedClass) {
+        selected_weapons.push(key);
+      }
+    });
+
+    return selected_weapons;
+  }
+
+  onItemSelected(event: any): void {
+    this.selectedClass = event.target.value;
+    this.getItemClassValues();
+  }
+
+  onWeaponSelected(event: any): void {
+    this.selectedCategory = event.target.value;
+  }
+
+  onTierSelected(event: any): void {
+    this.selectedTier = event.target.value;
   }
 }
